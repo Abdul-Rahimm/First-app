8fe5d74dbd4ca1ec86f355463fff0e1cc05bd947
Abdul-Rahimm <k213951@nu.edu.pk>
2024-06-06 12:05:50 +0500

    dynamic linking

--- nextapp/src/app/products/page.tsx
+++ nextapp/src/app/products/page.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 
 export default function ProductList() {
+  const productID = 100;
+
   return (
     <>
       <Link href={"/"}>
         <h2>Home </h2>
       </Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href={"/products/1"} replace>
+          Product 1
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${productID}`}>Product {productID}</Link>
+      </h2>
     </>
   );
 }
