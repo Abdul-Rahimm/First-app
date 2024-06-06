32e667a9f8d20c0dc61fdb2bcb751ea5e82e5591
Abdul-Rahimm <k213951@nu.edu.pk>
2024-06-06 11:54:06 +0500

    basic usage of the Link component

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -1,5 +1,13 @@
-export default function Home(){
-    return <>
-        <h1 className="text-center">Hello Abdul Rahim to NextJS</h1>
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-center">Hello Abdul Rahim to NextJS</h1>
+      <Link href={"/blog"}>Blog</Link>
+      <Link href={"/products"}>
+        <h1>Products</h1>
+      </Link>
     </>
+  );
 }
--- nextapp/src/app/products/page.tsx
+++ nextapp/src/app/products/page.tsx
@@ -1,9 +1,15 @@
-export default function ProductList(){
-    return <>
-        <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
-        
+import Link from "next/link";
+
+export default function ProductList() {
+  return (
+    <>
+      <Link href={"/"}>
+        <h2>Home </h2>
+      </Link>
+      <h1>Product List</h1>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
     </>
+  );
 }
