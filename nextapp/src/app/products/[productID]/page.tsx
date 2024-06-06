479234176bd7928d480139b02727824df39103a7
Abdul-Rahimm <k213951@nu.edu.pk>
2024-06-06 11:34:55 +0500

    lesson 17 - metadata (static and dynamic)

--- nextapp/src/app/products/[productID]/page.tsx
+++ nextapp/src/app/products/[productID]/page.tsx
@@ -1,7 +1,26 @@
-export default function ProductDetails({params}:{
-    params : {productID : string}
-}){
-    return <h1>
-        Details about Product {params.productID}
-    </h1>
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+  params: {
+    productID: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productID}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
+  return <h1>Details about Product {params.productID}</h1>;
 }
