8cb4b0275887e2a7726dde699b5cddac61c7cd78
Abdul-Rahimm <k213951@nu.edu.pk>
2024-06-12 00:42:05 +0500

    learned static basic metadata generation. dynamic can be learned easily

--- js_mastery/app/user/page.js
+++ js_mastery/app/user/page.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import User from '@/components/user';
 
+export const metadata = {
+  title:"User Page"
+}
+
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-row items-center justify-center bg-gray-100">
